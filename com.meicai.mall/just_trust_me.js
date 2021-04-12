d60b51013360c8890d155f8dd75ca6e4dc3d5009
郭钟 <guozhong@quicklyun.com>
2021-04-12 21:50:01 +0800

    fix just_trust_me.js

--- com.meicai.mall/just_trust_me.js
+++ com.meicai.mall/just_trust_me.js
@@ -9,12 +9,31 @@ function classExists(className) {
     return exists;
 };
 
+function loadDexfile(dexfile) {
+    Java.perform(function() {
+          Java.openClassFile(dexfile).load();
+          //console.log("load " + dexfile);
+    });
+};
+
+var loadedXRadar = false;
+function loadXRadarDexfile() {
+    loadedXRadar = true;
+    loadDexfile('/data/user/0/com.meicai.mall/radar.dex');
+};
 
 function getSCCM() {
     return Java.use("gz.justtrustme.Helper").getSCCM();
 }
 
+loadXRadarDexfile();
+
+function hasTrustManagerImpl() {
+    return classExists("com.android.org.conscrypt.TrustManagerImpl");
+}
+
 Java.perform(function() {
+    var Helper = Java.use("gz.justtrustme.Helper");
     if (classExists("org.apache.http.impl.client.DefaultHttpClient")) {
         var DefaultHttpClientClass = Java.use("org.apache.http.impl.client.DefaultHttpClient");
         //被强奸的构造方法
@@ -49,4 +68,51 @@ Java.perform(function() {
         //什么都不做
     };
     
+    //替换trustmanagers参数
+    var SSLSocketFactory = Java.use('org.apache.http.conn.ssl.SSLSocketFactory');
+    var SSLSocketFactoryRapeConstructor = SSLSocketFactory.$init.overload('java.lang.String', 'java.security.KeyStore', 'java.lang.String', 'java.security.KeyStore', 'java.security.SecureRandom', 'org.apache.http.conn.scheme.HostNameResolver');
+    SSLSocketFactoryRapeConstructor.implementation = function(v0, v1, v2, v3, v4, v5) {
+        var returnObj = SSLSocketFactoryRapeConstructor.call(this, v0, v1, v2, v3, v4, v5);
+        Helper.reInitSSLSocketFactory(returnObj, v0, v1, v2, v3, v4, v5);
+        return returnObj;
+    };
+
+    var SSLSocketFactoryGetSocketFactoryMethod = SSLSocketFactory.getSocketFactory.overload();
+    var SSLSocketFactoryEmptyConstructor = SSLSocketFactory.$init.overload();
+    SSLSocketFactoryGetSocketFactoryMethod.implementation = function() {
+        //强制用空的构造方法
+        return SSLSocketFactoryEmptyConstructor.call(this);
+    };
+
+
+    var SSLSocketFactoryIsSecure = SSLSocketFactory.isSecure.overload('java.net.Socket');
+    SSLSocketFactoryIsSecure.implementation = function(v0) {
+        //强制返回true
+        return true;
+    };
+
+    var TrustManagerFactory = Java.use('javax.net.ssl.TrustManagerFactory');
+    var TrustManagerFactoryGetTrustManagers = TrustManagerFactory.getTrustManagers.overload();
+    TrustManagerFactoryGetTrustManagers.implementation = function() {
+        var ret = TrustManagerFactoryGetTrustManagers.call(this);
+        //替换getTrustManagers方法逻辑
+        return Helper.replaceGetTrustManagers(this, ret);
+    };
+
+    var HttpsURLConnection = Java.use("javax.net.ssl.HttpsURLConnection");
+    var HttpsURLConnectionSetDefaultHostnameVerifier = HttpsURLConnection.setDefaultHostnameVerifier.overload('javax.net.ssl.HostnameVerifier');
+    HttpsURLConnectionSetDefaultHostnameVerifier.implementation = function(v0) {
+        //什么都不做
+    };
+
+    var HttpsURLConnectionSetHostnameVerifier = HttpsURLConnection.setHostnameVerifier.overload('javax.net.ssl.HostnameVerifier');
+    HttpsURLConnectionSetHostnameVerifier.implementation = function(v0) {
+        //什么都不做
+    };
+
+    var HttpsURLConnectionSetSSLSocketFactory = HttpsURLConnection.setSSLSocketFactory.overload('javax.net.ssl.SSLSocketFactory');
+    HttpsURLConnectionSetSSLSocketFactory.implementation = function(v0) {
+        //什么都不做
+    };
+
 })
